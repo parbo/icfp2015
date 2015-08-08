a82be7e9269eada9af3f4ff0cbb1d54ed1efe8e8
Pär Bohrarper <par.bohrarper@gmail.com>
2015-08-08 17:28:29 +0200

    Add unit astar

--- src/test_astar.py
+++ src/test_astar.py
@@ -1,8 +1,8 @@
 import astar
+import game
 import hx
 
-
-if __name__=="__main__":
+def test_hex():
     def g(n1, n2):
         return 1
     def nf(g):
@@ -23,4 +23,33 @@ if __name__=="__main__":
     start = hx.to_hex(0, 0)
     goal = hx.to_hex(6, 7)
     f, p = astar.astar(start, goal, g, hf(goal), nf(None))
+    print f, [hx.to_offset(x) for x in p]
+
+def test_units():
+    def g(n1, n2):
+        return 1
+    def nf(g):
+        def neighbours(u):
+            nb = []
+            for d in ["E", "W", "SE", "SW", "CW", "CCW"]:
+                new_u = u.action(d)
+                col, row = new_u.pivot
+                if 0 <= col < 10 and 0 <= row < 10:
+                    nb.append(new_u)
+            print u.pivot, u.members, [(x.pivot, x.members) for x in nb]
+            return nb
+        return neighbours
+    def hf(goal):
+        def h(n):
+            gc, gr = goal.pivot
+            nc, nr = n.pivot
+            return hx.hex_dist(hx.to_hex(gc, gr), hx.to_hex(nc, nr))
+        return h
+    start = game.Unit((0, 0), [(0, 0), (1, 0)])
+    goal = game.Unit((1, 6), [(1, 6), (1, 7)])
+    f, p = astar.astar(start, goal, g, hf(goal), nf(None))
     print f, p
+
+if __name__=="__main__":
+    test_hex()
+    test_units()
