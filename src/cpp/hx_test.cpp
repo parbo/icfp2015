291b3b1b89ac2523f508bd9daa2afdf372d1b630
Pär Bohrarper <par.bohrarper@gmail.com>
2015-09-05 13:20:22 +0200

    Use cmake to build and boost for unit tests.

--- src/cpp/hx_test.cpp
+++ src/cpp/hx_test.cpp
@@ -3,12 +3,13 @@
 #include <cassert>
 #include <iostream>
 
-int main() {
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(distance) {
   const auto h0 = hx::to_hex(2, 2);
-  assert(0 == hx::distance(h0, h0));
+  BOOST_CHECK_EQUAL(0, hx::distance(h0, h0));
   const auto h1 = hx::to_hex(1, 3);
-  assert(1 == hx::distance(h0, h1));
+  BOOST_CHECK_EQUAL(1, hx::distance(h0, h1));
   const auto h2 = hx::to_hex(4, 5);
-  std::cout << hx::distance(h0, h2) << std::endl;
-  assert(4 == hx::distance(h0, h2));
+  BOOST_CHECK_EQUAL(4, hx::distance(h0, h2));
 }
